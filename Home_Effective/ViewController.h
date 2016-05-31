447ee1eec808408e0da3eca5e2852537f5d193ce
liudukun <ldk@liudukun.local>
2016-05-31 15:57:32 +0800

    Initial Commit

---
+++ Home_Effective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Home_Effective
+//
+//  Created by liudukun on 16/5/31.
+//  Copyright © 2016年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Home_Effective/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Home_Effective
+//
+//  Created by liudukun on 16/5/31.
+//  Copyright © 2016年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
